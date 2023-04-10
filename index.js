f437efa7e6f8b6dbe67d4c22c56edff100c4bd11
Rayyan <rayyanrsarkhot@gmail.com>
2023-04-10 15:21:13 -0400

    Added endpoint to update all parts of an envelope.

--- index.js
+++ index.js
@@ -16,6 +16,29 @@ app.get('/envelopes', (req, res, next) => {
     res.status(200).send(envelopes);
 });
 
+// Get endpoint to receive a specific envelope.
+app.get('/envelopes/:name', (req,res,next) => {
+   
+    // Flag indicates whether envelope exists in array.
+    let index = -1;
+    
+    // Loops through envelopes array to check if envelope exists.
+    for(let i = 0; i < envelopes.length; i++) {
+        if(envelopes[i].name === req.params.name.toLowerCase()){ 
+            index = i;
+        }
+    }
+
+    // Checks if envelope does not exists.
+    if(index === -1) {
+        res.status(400).send('ENVELOPE DOES NOT EXIST');
+    } else {
+        // Sends specific envelope back.
+        res.status(200).send(envelopes[index]);   
+    }
+
+});
+
 // Post endpoint to add an envelope.
 app.post('/envelopes/add', (req,res,next) => {
     
@@ -53,6 +76,54 @@ app.post('/envelopes/add', (req,res,next) => {
         res.status(201).send(envelopes[envelopes.length-1]);
 
     }
+
+});
+
+// Put endpoint to call to update an envelope.
+app.put('/envelopes/update/:name', (req,res,next) => {
+        
+    // Flag indicates whether envelope exists in array.
+    let index = -1;
+    
+    // Loops through envelopes array to check if envelope exists.
+    for(let i = 0; i < envelopes.length; i++) {
+        if(envelopes[i].name === req.params.name.toLowerCase()){ 
+            index = i;
+        }
+    }
+
+    // Checks if envelope does not exists.
+    if(index === -1) {
+        res.status(400).send('ENVELOPE DOES NOT EXIST');
+    } else {
+
+        // If statement checks if user's used query is a valid value.
+        if(Number(req.query.used)) {
+            envelopes[index].used += Number(req.query.used);// Increases used value.   
+        } 
+        // This fires if the user entered an invalid number. Not if it hasn't been entered.
+        else if(req.query.used !== undefined) {
+            res.status(400).send('USED VALUE IS NOT A NUMBER');
+        }
+
+        // If statement checks if user's budget query is a valid value.
+        if(Number(req.query.budget)) {
+            envelopes[index].budget = Number(req.query.budget);// Sets budget.    
+        } 
+        // This fires if the user entered an invalid number. Not if it hasn't been entered.
+        else if(req.query.budget !== undefined) {
+            res.status(400).send('BUDGET VALUE IS NOT A NUMBER');
+        }
+
+        // If statement checks if user sent in a value for renaming.
+        if(req.query.nombre !== undefined) {
+            envelopes[index].name = req.query.nombre// Sets name.    
+        }
+        
+        // Responds to client with remaining balance.
+        res.status(200).send('ENVELOPE SUCCESSFULLY UPDATED.');         
+        
+        }
 
 });
 
